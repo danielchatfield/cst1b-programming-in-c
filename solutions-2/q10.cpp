bf46f7c7b0123aa140db06b36d45934be0d97ce0
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-12-01 17:56:39 +0000

    Refactored so it will actually compile

--- solutions-2/q10.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "stack.h"
-
-template <class T> Stack<T>::Stack() {
-
-}
--- solutions-2/stack.h
+++ solutions-2/stack.h
@@ -1,3 +1,5 @@
+#include <cstdio>
+
 template <class T> class Stack {
 
     struct Item {
@@ -9,8 +11,8 @@ template <class T> class Stack {
 
     Item* head;
 
-    Stack(const Stack& s) {}
-    Stack& operator=(const Stack& s) {}
+    Stack(const Stack& s);
+    Stack& operator=(const Stack& s);
 
 public:
     Stack() : head(0) {}
@@ -19,3 +21,47 @@ public:
     void push(T val);
     void append(T val);
 };
+
+template<class T> void Stack<T>::append(T val) {
+    Item **pp = &head;
+    while(*pp) {
+        pp = &((*pp)->next);
+    }
+    *pp = new Item(val);
+}
+
+int main() {
+    Stack<char> s;
+    s.push('a'), s.append('b'), s.pop();
+}
+
+class EmptyStackError{};
+
+template<class T> void Stack<T>::push(T val) {
+    this->head = new Item(val, this->head);
+}
+
+template<class T> T Stack<T>::pop() {
+    if (this->head == NULL) {
+        throw EmptyStackError();
+    }
+    Item *rv = this->head;
+    this->head = this->head->next;
+    return rv->val;
+}
+
+template<class T> Stack<T>::~Stack() {
+    Item **pp = &this->head; // take a pointer to the head pointer
+    while(*pp) { // while the pointer is still pointing to an item:
+        Item &rv = **pp; // take a reference to the current item
+        pp = &rv.next; //move pp onto the next one
+
+        // delete current one (not sure whether need to explicitly delete the data)
+        delete rv;
+    }
+}
+
+
+template <class T> Stack<T>::Stack(const Stack& s) {
+
+}
